8040f613c72962602b6cfda6e27ec6e0e8910e21
Pascal Rey <pascalrey1955@gmail.com>
2022-06-05 13:12:37 -0600

    setup accommodation mvc structures

---
+++ app/controllers/accommodation_controller.rb
@@ -0,0 +1,10 @@
+class AccommodationController < ApplicationController
+  def index
+  end
+
+  def sea_esta
+  end
+
+  def la_palma
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'homepage', to: 'homepage#index'
-  get 'accommodation', to: 'homepage#accommodation'
+  get 'accommodation', to: 'accommodation#index'
+  get 'accommodation', to: 'accommodation#sea_esta'
+  get 'accommodation', to: 'accommodation#la_palma'
   root 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
