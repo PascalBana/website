4932c7fadc17764a12b9ff236acfefcecd40f71e
Pascal Rey <pascalrey1955@gmail.com>
2022-06-06 08:35:27 -0600

    fixed minor issues from reorganizing css

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_directory ../stylesheets/sitewide .css
+//= link_directory ../stylesheets/homepage .css
+//= link_directory ../stylesheets/accommodation .css
