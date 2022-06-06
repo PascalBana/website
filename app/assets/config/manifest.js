0e4728c37bb397fe1285a6da47217dce0338c1d2
Pascal Rey <pascalrey1955@gmail.com>
2022-06-06 16:12:34 -0600

    started on seaesta page

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,4 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_directory ../stylesheets/sitewide .css
 //= link_directory ../stylesheets/homepage .css
 //= link_directory ../stylesheets/accommodation .css
