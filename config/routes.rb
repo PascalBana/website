eb6f589ba15622e908d57e9bc143db5b9ecf7a53
Pascal Rey <pascalrey1955@gmail.com>
2022-06-06 08:19:16 -0600

    seperated header and footer into partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'homepage', to: 'homepage#index'
   get 'accommodation', to: 'accommodation#index'
-  get 'accommodation', to: 'accommodation#sea_esta'
-  get 'accommodation', to: 'accommodation#la_palma'
+  get 'sea-esta', to: 'accommodation#sea_esta'
+  get 'la-palma', to: 'accommodation#la_palma'
   root 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
